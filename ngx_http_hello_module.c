9917f89c955139766c5da581cae78cdf640a4626
Usama Dar <munir.usama@gmail.com>
2012-09-02 16:15:28 +0200

    added readme

---
+++ ngx_http_hello_module.c
@@ -0,0 +1,154 @@
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+ 
+
+static char *ngx_http_hello(ngx_conf_t *cf, void *post, void *data);
+
+static ngx_conf_post_handler_pt ngx_http_hello_p = ngx_http_hello;
+
+typedef struct {
+    ngx_str_t   name;
+} ngx_http_hello_loc_conf_t;
+
+static void *
+ngx_http_hello_create_loc_conf(ngx_conf_t *cf)
+{
+    ngx_http_hello_loc_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+    
+    return conf;
+}
+
+static ngx_command_t ngx_http_hello_commands[] = {
+    { ngx_string("hello"),
+      NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      ngx_conf_set_str_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_hello_loc_conf_t, name),
+      &ngx_http_hello_p },
+ 
+    ngx_null_command
+};
+ 
+ 
+static ngx_str_t hello_string;
+ 
+ 
+static ngx_http_module_t ngx_http_hello_module_ctx = {
+    NULL,                          /* preconfiguration */
+    NULL,                          /* postconfiguration */
+ 
+    NULL,                          /* create main configuration */
+    NULL,                          /* init main configuration */
+ 
+    NULL,                          /* create server configuration */
+    NULL,                          /* merge server configuration */
+ 
+    ngx_http_hello_create_loc_conf, /* create location configuration */
+    NULL                           /* merge location configuration */
+};
+ 
+ 
+ngx_module_t ngx_http_hello_module = {
+    NGX_MODULE_V1,
+    &ngx_http_hello_module_ctx,    /* module context */
+    ngx_http_hello_commands,       /* module directives */
+    NGX_HTTP_MODULE,               /* module type */
+    NULL,                          /* init master */
+    NULL,                          /* init module */
+    NULL,                          /* init process */
+    NULL,                          /* init thread */
+    NULL,                          /* exit thread */
+    NULL,                          /* exit process */
+    NULL,                          /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+ 
+ 
+static ngx_int_t
+ngx_http_hello_handler(ngx_http_request_t *r)
+{
+    ngx_int_t    rc;
+    ngx_buf_t   *b;
+    ngx_chain_t  out;
+ 
+    /* we response to 'GET' and 'HEAD' requests only */
+    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+ 
+    /* discard request body, since we don't need it here */
+    rc = ngx_http_discard_request_body(r);
+ 
+    if (rc != NGX_OK) {
+        return rc;
+    }
+ 
+    /* set the 'Content-type' header */
+    r->headers_out.content_type_len = sizeof("text/html") - 1;
+    r->headers_out.content_type.len = sizeof("text/html") - 1;
+    r->headers_out.content_type.data = (u_char *) "text/html";
+ 
+    /* send the header only, if the request type is http 'HEAD' */
+    if (r->method == NGX_HTTP_HEAD) {
+        r->headers_out.status = NGX_HTTP_OK;
+        r->headers_out.content_length_n = hello_string.len;
+ 
+        return ngx_http_send_header(r);
+    }
+ 
+    /* allocate a buffer for your response body */
+    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+ 
+    /* attach this buffer to the buffer chain */
+    out.buf = b;
+    out.next = NULL;
+ 
+    /* adjust the pointers of the buffer */
+    b->pos = hello_string.data;
+    b->last = hello_string.data + hello_string.len;
+    b->memory = 1;    /* this buffer is in memory */
+    b->last_buf = 1;  /* this is the last buffer in the buffer chain */
+ 
+    /* set the status line */
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = hello_string.len;
+ 
+    /* send the headers of your response */
+    rc = ngx_http_send_header(r);
+ 
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+        return rc;
+    }
+ 
+    /* send the buffer chain of your response */
+    return ngx_http_output_filter(r, &out);
+}
+ 
+ 
+static char *
+ngx_http_hello(ngx_conf_t *cf, void *post, void *data)
+{
+    ngx_http_core_loc_conf_t *clcf;
+
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_hello_handler;
+
+    ngx_str_t  *name = data; // i.e., first field of ngx_http_hello_loc_conf_t
+    
+    if (ngx_strcmp(name->data, "") == 0) {
+        return NGX_CONF_ERROR;
+    }
+    hello_string.data = name->data;
+    hello_string.len = ngx_strlen(hello_string.data);
+
+    return NGX_CONF_OK;
+}
